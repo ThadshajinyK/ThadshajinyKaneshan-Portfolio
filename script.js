728c8b772fd22e4e58dc6c9f516063dec497daa0
ThadshajinyK <kaneshanthadshajiny@gmail.com>
2023-11-05 16:28:10 +0530

    50% completed

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+
+    const menu = document.querySelector(".humburger-links");
+    const icon = document.querySelector(".humburger-icon");
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
